2f8c1e49cec82a08d20ea79421808e4736e1879c
veronicajujuy <veronica.jujuy@gmail.com>
2023-07-28 17:22:35 -0300

    agregando proyecto mercadoliebre

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.static("public"));
+
+app.listen(3001, () => console.log("Servidor corriendo en puerto 3001"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/home.html"));
+});
