06e8be355d30d3cf08ba1e728cec019ca45179af
veronicajujuy <veronica.jujuy@gmail.com>
2023-08-08 00:31:25 -0300

    agregando cmabios

--- src/app.js
+++ src/app.js
@@ -9,3 +9,6 @@ app.listen(3001, () => console.log("Servidor corriendo en puerto 3001"));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./views/home.html"));
 });
+app.get("/registro", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/registro.html"));
+});
